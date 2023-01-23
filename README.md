96145fa34229ee7ce57343a99be9d4689cc0bdcc
PedroMion <pedrombc@dcc.ufrj.br>
2023-01-23 18:14:37 -0300

    finishing JS

---
+++ README.md
@@ -0,0 +1,10 @@
+# Javascript-letter-counter
+
+This project receives a phrase from the user and give the number of letters or the most used letter
+
+## 💻 Tecnologies
+- JavaScript
+
+## 🧔 Website
+
+https://2024-javascript-countdown.netlify.app
--- script.js
+++ script.js
@@ -1,19 +1,65 @@
-const letters = new Map();
-
 const first_button = document.querySelector('#numberofletters');
 const second_button = document.querySelector('#mostusedletter');
+const return_button = document.querySelector('#popupbutton');
+const popup_text = document.querySelector('#popuptext');
+const popup = document.querySelector('.popup');
 
-const letters_array = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const letters_array = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+const ret_button = () => {
+
+    popup.style.display = 'none';
+    
+}
+
+const result = (bool, value) => {
+
+    if(bool) {
+    
+        popup_text.textContent = 'Total number of letters is ' + value;
+
+    } else {
+
+        popup_text.textContent = 'Most used letter is ' + value;
+
+    }
+
+    popup.style.display = 'flex';
+
+}
 
 const number_letters = () => {
+
     var phrase = document.querySelector('#userphrase').value;
     var count = phrase.length - phrase.split(' ').length + 1;
-    console.log(count);
+    
+    result(true, count);
+
 }
 
 const most_used = () => {
-    console.log(letters_array);
+
+    var phrase = document.querySelector('#userphrase').value;
+    var quantity = new Array(26).fill(0);
+    var index = 0;
+
+    for (var i = 0; i<phrase.length; i++) {
+
+        if (letters_array.includes(phrase[i])) {
+
+            index = letters_array.indexOf(phrase[i]);
+            quantity[index%26] += 1
+
+        }
+
+    }
+    
+    const mostUsed = letters_array[quantity.indexOf(Math.max(...quantity))]
+    
+    result(false, mostUsed);
+
 }
 
 first_button.addEventListener("click", number_letters);
 second_button.addEventListener("click", most_used);
+return_button.addEventListener("click", ret_button);
